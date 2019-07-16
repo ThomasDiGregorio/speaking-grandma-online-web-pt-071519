2afee44e5b311ad1a81de6225e7120efb08b0a27
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-07-16 03:46:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(sentence)
-  if sentence == sentence.upcase
-    "NO, NOT SINCE 1938!"
-  elsif sentence ==  "I LOVE YOU GRANDMA!"
+   if sentence ==  "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
+    
   elsif sentence == sentence.downcase
     "HUH?! SPEAK UP, SONNY!"
     
