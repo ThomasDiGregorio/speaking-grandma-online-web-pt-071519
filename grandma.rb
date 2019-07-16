c06203724224a12ca225dedb78dabe1e430d5822
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-07-16 03:47:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,8 @@
 def speak_to_grandma(sentence)
    if sentence ==  "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-    
+  elsif sentence == sentence.upcase
+    "NO, NOT SINCE 1938!"
   elsif sentence == sentence.downcase
     "HUH?! SPEAK UP, SONNY!"
     
